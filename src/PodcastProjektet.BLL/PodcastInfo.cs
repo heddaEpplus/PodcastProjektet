ae97e008b079bdbc6b856d20befc3e33dc229e01
hedda231 <heddaalmsmail@gmail.com>
2024-10-21 14:09:08 +0200

    wip

---
+++ src/PodcastProjektet.BLL/PodcastInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodcastProjektet.BLL
+{
+    public class PodcastInfo
+    {
+        public string Id { get; set; }
+        public int AntalAvsnitt { get; set; }
+        public string? Namn { get; set; }
+        public string? Titel { get; set; }
+        public string? Frekvens { get; set; }
+        public string? Kategori { get; set; }
+
+        public PodcastInfo()
+        {
+
+        }
+        public PodcastInfo(string Id, int AntalAvsnitt, string Namn, string Titel, string Frekvens, string Kategori)
+        {
+            this.Id = Id;
+            this.AntalAvsnitt = AntalAvsnitt;
+            this.Namn = Namn;
+            this.Titel = Titel;
+            this.Frekvens = Frekvens;
+            this.Kategori = Kategori;
+        }
+    }
+}
--- src/PodcastProjektet.PL/Form1.cs
+++ src/PodcastProjektet.PL/Form1.cs
@@ -1,10 +1,31 @@
+
+using PodcastProjektet.BLL;
+
 namespace PodcastProjektet.PL
 {
     public partial class Form1 : Form
     {
+        private PodcastManager _podcastManager;
         public Form1()
         {
             InitializeComponent();
+            _podcastManager = new PodcastManager();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(_podcastManager.getNamn());
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
