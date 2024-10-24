278ea3cf1e0b44e52097c8a0f871e71b09f5ef04
NoraBolin <norabolin@hotmail.com>
2024-10-24 14:42:05 +0200

    wip

--- src/PodcastProjektet.BLL/kategoriController.cs
+++ src/PodcastProjektet.BLL/kategoriController.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PodcastProjektet.DAL;
+using PodcastProjektet.DAL.Repository;
 
 namespace PodcastProjektet.BLL
 {
-    internal class kategoriController
+    public class KategoriController
     {
+        CategoryRepository _categoryRepository;
+
+        public KategoriController()
+        {
+            _categoryRepository = new CategoryRepository();
+        }
+
+        public List<string> HamtaAllaKategorier()
+        {
+            List<string> allaKategorier = new List<string>();
+            allaKategorier = _categoryRepository.HamtaAllaKategorier();
+            return allaKategorier;
+        }
+
+        public void LaggTillKategori(string nyKategori)
+        {
+            _categoryRepository.LaggTillKategori(nyKategori);
+        }
+
+        public void SkappaDefaultKategori()
+        {
+            _categoryRepository.SkappaDefaultKategori();
+        }
+
+        public void TaBort(string kategori)
+        {
+            _categoryRepository.TaBort(kategori);
+        }
+
+        public void Uppdatera(string kategoriNamn, string nyKategoriNamn)
+        {
+            _categoryRepository.Uppdatera(kategoriNamn, nyKategoriNamn);
+        }
+
+        public void Uppdatera(string kategoriNamn, string nyKategoriNamn, string podTitel)
+        {
+            _categoryRepository.Uppdatera(kategoriNamn, nyKategoriNamn, podTitel);
+        }
     }
 }
